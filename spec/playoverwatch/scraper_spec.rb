c751cdd2627f046076e33d57bede1c020a7b66ef
Duncan Leo <duncanleo97@gmail.com>
2018-08-04 19:16:58 +0800

    update references to generated namespaces

--- spec/playoverwatch/scraper_spec.rb
+++ spec/playoverwatch/scraper_spec.rb
@@ -1,6 +1,6 @@
-RSpec.describe Playoverwatch::Scraper do
+RSpec.describe PlayOverwatch do
   it "has a version number" do
-    expect(Playoverwatch::Scraper::VERSION).not_to be nil
+    expect(PlayOverwatch::VERSION).not_to be nil
   end
 
   it "does something useful" do
