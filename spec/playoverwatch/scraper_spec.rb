527ed236c80caffa59d9ff39f914d13d63e977d0
Duncan Leo <duncanleo97@gmail.com>
2018-08-05 13:01:04 +0800

    add tests

--- spec/playoverwatch/scraper_spec.rb
+++ spec/playoverwatch/scraper_spec.rb
@@ -3,7 +3,29 @@ RSpec.describe PlayOverwatch do
     expect(PlayOverwatch::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "can scrape correctly" do
+    before(:all) do
+      @scraper = PlayOverwatch::Scraper.new('BattleTag#4615') # Random battle tag from Google
+    end
+
+    it "can get player icon" do
+      expect(@scraper.player_icon).to start_with('http')
+    end
+
+    it "can get player level" do
+      expect(@scraper.player_level).to be_an(Numeric)
+    end
+
+    it "can get endorsement level" do
+      expect(@scraper.endorsement_level).to be_an(Numeric)
+    end
+
+    it "can get competitive SR" do
+      expect(@scraper.sr).to be_an(Numeric)
+    end
+
+    it "can get main qp" do
+      expect(@scraper.main_qp).to be_a(String)
+    end
   end
 end
